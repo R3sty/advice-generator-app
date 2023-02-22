441f132fe873485a24434c215c9ddd5608454bdb
R3sty <iral.resty@gmail.com>
2023-02-22 23:58:01 +0900

    added tailwindcss, set up github pages

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		screens: {
+			desktop: '1440px',
+			mobile: '375px',
+		},
+		colors: {
+			red: '#FF0000',
+		},
+		fontFamily: {},
+		fontSize: {
+			l: ['40px', { lineHeight: '40px', fontWeight: 700 }],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
